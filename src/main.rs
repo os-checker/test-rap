16b967eab7200acaf2a28cc6f4198dfefab21a92
zjp <jiping_zhou@foxmail.com>
2024-11-27 11:17:32 +0800

    reproduce: Double free detected in function main:13:5: 13:6 (#0)

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ fn main() {
             Err(err) => {
                 eprintln!("{err}");
             }
-            Ok(a) => _ = a, // no double free by rap
+            // Ok(a) => _ = a, // no double free by rap
 
-                            // Ok(_) =>(), // double free by rap
-                            // _ =>(),// double free by rap
+            // Ok(_) =>(), // double free by rap
+            _ => (), // double free by rap
         }
     }
 }
+// 11:17:09|RAP|WARN|: Double free detected in function main
+// 11:17:09|RAP|WARN|: Location: src/main.rs:13:5: 13:6 (#0)
